d7eb5bd4e4fe24de7247d741e50f02960cfe68ba
Moises <moisesssbarbosa80@gmail.com>
2026-05-06 21:01:05 -0300

    feat: barra de busca e geração de relatorio

--- frontend/src/Recepcionista.jsx
+++ frontend/src/Recepcionista.jsx
@@ -1,5 +1,8 @@
 import api from './api';
-import React, { useEffect, useState } from 'react'; // Importando sua configuração do axios
+import { Form, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import  { useReactToPrint  } from 'react-to-print'// Importando sua configuração do axios
 
 export default function Recepcionista() {
 
@@ -31,41 +34,71 @@ export default function Recepcionista() {
     }
   };
 
+  const [busca, setBusca] = useState("");
+
+  const convidadosFiltrados = convidados.filter((c) => 
+    c.nome.toLowerCase().includes(busca.toLocaleLowerCase()) ||
+    c.email.toLowerCase().includes(busca.toLocaleLowerCase()) ||
+    String(c.mesa).includes(busca)
+  );
+ 
+  const componentRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    contentRef: componentRef,
+    documentTitle: 'Relatório', // Opcional: Define o nome do arquivo
+  });
+
   return (
+
         <div style={{ padding: '20px' }}>
-          <h1>Painel do Recepcionista 🔐</h1>   
+          <h1 style={{ marginBottom: '40px'}}>Painel do Recepcionista 🔐</h1>   
           
-          <table border="1" style={{ width: '100%', marginTop: '100px', textAlign: 'left' }}>
-            <thead>
-              <tr style={{ backgroundColor: '#f2f2f2' }}>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Telefone</th>
-                <th>Mesa</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              {convidados.map((c) => (
-                <tr key={c.id}>
-                  <td>{c.nome}</td>
-                  <td>{c.email}</td>
-                  <td>{c.telefone}</td>
-                  <td>{c.mesa}</td>
-                  <td>
-                    <label className="switch">
-                      <input 
-                        type="checkbox" 
-                        checked={c.status_checkin} 
-                        onChange={() => handleToggleCheckin(c.id, c.status_checkin)}
-                      />
-                      <span className="slider round"></span>
-                    </label>
-                  </td>
+          
+
+          <Form.Control
+            type="text"
+            placeholder="Buscar por nome ou e-mail..."
+            className="mb-3"
+            value={busca}
+            onChange={(e) => setBusca(e.target.value)}
+          />
+
+          <Button onClick={handlePrint}>Gerar PDF</Button> 
+    
+          <div ref={componentRef}>
+            <table border="1" style={{ width: '100%', marginTop: '100px', textAlign: 'left' }}>
+              <thead>
+                <tr style={{ backgroundColor: '#f2f2f2' }}>
+                  <th>Nome</th>
+                  <th>E-mail</th>
+                  <th>Telefone</th>
+                  <th>Mesa</th>
+                  <th>Ações</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {convidadosFiltrados.map((c) => (
+                  <tr key={c.id}>
+                    <td>{c.nome}</td>
+                    <td>{c.email}</td>
+                    <td>{c.telefone}</td>
+                    <td>{c.mesa}</td>
+                    <td>
+                      <label className="switch">
+                        <input 
+                          type="checkbox" 
+                          checked={c.status_checkin} 
+                          onChange={() => handleToggleCheckin(c.id, c.status_checkin)}
+                        />
+                        <span className="slider round"></span>
+                      </label>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
   );
 }
